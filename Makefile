22c8701c18aba3008fd1dba90c07d6388c7d38b3
Seth Hoenig <shoenig@duck.com>
2022-10-13 20:26:40 -0500

    ci: setup ci on gha

--- Makefile
+++ Makefile
@@ -7,8 +7,16 @@ GOTAGS ?= osusergo
 	@rm -f ./output/pledge
 
 .PHONY: dev
-dev: GOOS=$(shell go env GOOS)
-dev: GOARCH=$(shell go env GOARCH)
 dev: clean
 	@echo "==> Compile pledge plugin"
 	@go build -race -tags=$(GOTAGS) -o output/pledge
+
+.PHONY: test
+test:
+	@echo "==> Test pledge plugin"
+	@go test -race -v ./...
+
+.PHONY: vet
+vet:
+	@echo "==> Vet pledge packages"
+	@go vet ./...
--- pkg/pledge/exec_test.go
+++ pkg/pledge/exec_test.go
@@ -3,25 +3,23 @@ package pledge
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"os"
+	"os/user"
 	"testing"
 
+	"github.com/shoenig/nomad-pledge/pkg/resources"
 	"github.com/shoenig/test/must"
 )
 
 const (
 	pledgeEnvVar     = "PLEDGE_PATH"
-	defaultPledgeBin = "/opt/bin/pledge.com"
-)
-
-const (
-	binPledge    = "pledge"
-	binPledgeCom = "pledge.com"
+	defaultPledgeBin = "/opt/bin/pledge-1.8.com"
 )
 
 func lookupBin() string {
-	if env := os.Getenv(pledgeEnvVar); env == "" {
+	if env := os.Getenv(pledgeEnvVar); env != "" {
 		return env
 	}
 	return defaultPledgeBin
@@ -39,16 +37,25 @@ func noopCloser(w io.Writer) io.WriteCloser {
 	return &writeCloser{w}
 }
 
+func whoami() string {
+	u, err := user.Current()
+	if err != nil {
+		panic("unable to lookup current user")
+	}
+	return u.Username
+}
+
 func testEnv() (*Environment, *bytes.Buffer, *bytes.Buffer) {
 	var out bytes.Buffer
 	var err bytes.Buffer
 
 	return &Environment{
-		Out:  noopCloser(&out),
-		Err:  noopCloser(&err),
-		Env:  map[string]string{},
-		Dir:  ".",
-		User: "shoenig",
+		Out:    noopCloser(&out),
+		Err:    noopCloser(&err),
+		Env:    map[string]string{},
+		Dir:    ".",
+		User:   whoami(),
+		Cgroup: fmt.Sprintf("/sys/fs/cgroup/user.slice/%s.%s.scope", "abc123", "test"),
 	}, &out, &err
 }
 
@@ -56,16 +63,23 @@ func testOpts() *Options {
 	return &Options{
 		Command:   "echo",
 		Arguments: []string{"hello", "world"},
+		Importance: &resources.Importance{
+			Label: "low",
+			Nice:  10,
+		},
 	}
 }
 
 func TestExec_hello(t *testing.T) {
+	t.Skip("requires refactoring to use mocks")
+
 	env, stdout, stderr := testEnv()
+	t.Log("whoami user is", env.User)
 	opts := testOpts()
 	e := New(lookupBin(), env, opts)
 	ctx := context.Background()
 	must.NoError(t, e.Start(ctx))
 	must.NoError(t, e.Wait())
-	must.ContainsString(t, stdout.String(), "hello")
+	must.StrContains(t, stdout.String(), "hello")
 	must.Eq(t, "", stderr.String())
 }
